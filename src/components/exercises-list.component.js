a3e749cc57e73b27e887940d3e36a4f388cee87e
Mico Cataga <mcataga@ryerson.ca>
2020-05-17 06:38:39 -0400

    distance calculation finished

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import GoogleMaps from './google-maps.component';
+import { getDistance } from 'geolib';
 import axios from 'axios';
 
 const Listing = props => (
@@ -15,7 +16,7 @@ const Listing = props => (
       <Card.Footer><Card.Title>Email: {props.listing.email}</Card.Title></Card.Footer>
       <Card.Footer><Card.Title>Number: {props.listing.number} </Card.Title></Card.Footer>
       <Card.Footer><Card.Title>Description: {props.listing.additional}</Card.Title></Card.Footer>
-      
+      <Card.Footer><Card.Title>Distance: {props.listing.distance}km</Card.Title></Card.Footer>
       <Card.Footer class="admin-background">
       <Button variant="accept" style={{float: 'right'}}>Accept</Button>
       </Card.Footer>
@@ -29,7 +30,9 @@ export default class ExercisesList extends Component {
 
     this.deleteListing = this.deleteListing.bind(this)
 
-    this.state = { listings: [] };
+    this.state = { listings: [], 
+                    userLong: '', 
+                    userLat: '' };
   }
 
   componentDidMount() {
@@ -43,10 +46,15 @@ export default class ExercisesList extends Component {
       .catch((error) => {
         console.log(error);
       })
-      navigator.geolocation.getCurrentPosition(function(position) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({userLong: position.coords.longitude, userLat: position.coords.latitude})
         console.log("Latitude is :", position.coords.latitude);
         console.log("Longitude is :", position.coords.longitude);
       });
+  }
+
+  getUserCoords() {
+    return {latitude: this.state.userLat, longitude: this.state.userLong};
   }
 
   deleteListing(id) {
@@ -60,6 +68,8 @@ export default class ExercisesList extends Component {
 
   listingList() {
     return this.state.listings.map(currentlisting => {
+      var distance = Math.abs(getDistance({latitude: this.state.userLat, longitude: this.state.userLong}, {latitude: currentlisting.latitude, longitude: currentlisting.longitude})/1000);
+      currentlisting.distance = Math.round(distance * 10)/10
       return <Listing listing={currentlisting} deleteListing={this.deleteListing} key={currentlisting._id} />;
     })
   }
